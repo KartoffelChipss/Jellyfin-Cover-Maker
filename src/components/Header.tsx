6418994b283581b31bde4fbaa434f8724c139a13
KartoffelChips <104089082+KartoffelChipss@users.noreply.github.com>
2025-08-03 20:29:14 +0200

    Some seo improvements

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,7 +3,11 @@ import GitHub from "./GitHub";
 const Header = () => (
     <header className="flex items-center justify-between w-full">
         <h1 className="text-4xl font-bold my-8 w-full flex items-center gap-2">
-            <img src={"/logo.svg"} alt={"Logo"} className="h-9" />
+            <img
+                src={"/logo.svg"}
+                alt={"Jellyfin Cover Maker Logo"}
+                className="h-9"
+            />
             Jellyfin Cover Maker
         </h1>
         <a
